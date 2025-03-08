fff8b201a3ca6b83cb50b0769cead2c9dfed273e
John Peña <john.pena@udemy.com>
2025-03-08 15:02:24 -0500

    feat: change favicon
    Signed-off-by: John Peña <john.pena@udemy.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🧸</text></svg>"
+        />
         <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
       <body className="antialiased bg-white dark:bg-gray-900">
